e7d30c6c85db9550552d0e430ccb9e1f29525114
AmishLou <rybread354@aol.com>
2014-06-28 21:28:56 -0500

    Commit Add2Integers

--- JavaPractice/src/Add2Integers.java
+++ JavaPractice/src/Add2Integers.java
@@ -1,4 +1,17 @@
+/*
+* File: Add2Integers.java
+* -----------------------
+* This program adds two integers and prints their sum.
+*/
 
-public class Add2Integers {
+import acm.program.*;
 
+public class Add2Integers extends ConsoleProgram {
+	public void run() {
+		println("This program adds two integers.");
+		int n1 = readInt("Enter n1: ");
+		int n2 = readInt("Enter n2: ");
+		int total = n1 + n2;
+		println("The total is " + total + ".");
+	}
 }
