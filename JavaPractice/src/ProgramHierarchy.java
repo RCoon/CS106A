2324a6a16de9f3f71e0d72cac49d711cdb7b9b07
AmishLou <rybread354@aol.com>
2014-07-05 18:05:24 -0500

    Final change

--- JavaPractice/src/ProgramHierarchy.java
+++ JavaPractice/src/ProgramHierarchy.java
@@ -12,122 +12,103 @@ import java.awt.*;
 
 public class ProgramHierarchy extends GraphicsProgram {	
 	
-	public static final double RECT_WIDTH = 150;
-	public static final double RECT_HEIGHT = 60;
+	// Width of each box in the picture
+	private static final double RECT_WIDTH = 150;
+	
+	// Height of each box in the picture
+	private static final double RECT_HEIGHT = 60;
+	
+	// Space between boxes
+	private static final double BOX_BUFFER = 20;
 	
 	public void run() {
-		
-		drawProgramHierarchy();
+		drawProgramBox();
+		drawBottomRow();
+		drawLines();
+		bottomLabels();
 	}
-
-	public double leftOfCenter() {
+	private double leftOfCenter() {
 		return getWidth() / 2.0 - RECT_WIDTH / 2.0;
 	}
-	
-	public void drawTopRow() {
-
+	private void drawProgramBox() {
 		double x = leftOfCenter();
-		double y = getHeight() / 4.0;
+		double y = getHeight() / 2.0 - RECT_HEIGHT;
 		GRect topRow = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
 		add(topRow);
-	}
-	
-	public void drawBottomRow() {
-		
-		double x = leftOfCenter() - RECT_WIDTH - 20;
-		double y = getHeight() / 2.0;
-		for (int i = 0; i < 3; i++) {
-			GRect lowerRow = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
-			add(lowerRow);
-			x += RECT_WIDTH + 20;
-		} 
-	}
-	
-	public void drawLines() {
-		drawLeft();
-		drawCenter();
-		drawRight();
-	}
-	
-	public void drawLeft() {
-		double x0 = getWidth() / 2.0;
-		double y0 = getHeight() / 4.0 + RECT_HEIGHT;
-		double x1 = leftOfCenter() - (RECT_WIDTH / 2.0 + 20);
-		double y1 = getHeight() / 2.0;
-		GLine left = new GLine(x0, y0, x1, y1);
-		add(left);
-	}
-	
-	public void drawCenter() {
-		double x0 = getWidth() / 2.0;
-		double y0 = getHeight() / 4.0 + RECT_HEIGHT;
-		double x1 = getWidth() / 2.0;
-		double y1 = getHeight() / 2.0;
-		GLine center = new GLine(x0, y0, x1, y1);
-		add(center);
-	}
-	
-	public void drawRight() {
-		double x0 = getWidth() / 2.0;
-		double y0 = getHeight() / 4.0 + RECT_HEIGHT;
-		double x1 = getWidth() / 2.0 + RECT_WIDTH + 20;
-		double y1 = getHeight() / 2.0;
-		GLine right = new GLine(x0, y0, x1, y1);
-		add(right);
-	}
-	
-	public void topLabel() {
-		double x = getWidth() / 2.0;
-		double y = getHeight() / 3.0;
 		GLabel label = new GLabel("Program", x,y);
 		add(label);
 		double x1 = label.getWidth() / 2.0;
 		double y1 = label.getAscent() / 2.0;
-		label.move(-x1, -y1);
+		label.move(RECT_WIDTH / 2.0 - x1, RECT_HEIGHT / 2.0 + y1);
 	}
-	
-	public void graphicsProgram() {
-		double x = leftOfCenter() - (RECT_WIDTH / 2.0 + 20);
-		double y = getHeight() / 1.725;
+	private void drawBottomRow() {
+		double x = leftOfCenter() - RECT_WIDTH - BOX_BUFFER;
+		double y = getHeight() / 2.0 + RECT_HEIGHT;
+		for (int i = 0; i < 3; i++) {
+			GRect lowerRow = new GRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+			add(lowerRow);
+			x += RECT_WIDTH + BOX_BUFFER;
+		} 
+	}
+	private void drawLines() {
+		drawLeft();
+		drawCenter();
+		drawRight();
+	}
+	private void drawLeft() {
+		double x0 = getWidth() / 2.0;
+		double y0 = getHeight() / 2.0;
+		double x1 = getWidth() / 2.0 - (RECT_WIDTH - BOX_BUFFER);
+		double y1 = getHeight() / 2.0 + RECT_HEIGHT;
+		GLine left = new GLine(x0, y0, x1, y1);
+		add(left);
+	}
+	private void drawCenter() {
+		double x0 = getWidth() / 2.0;
+		double y0 = getHeight() / 2.0;
+		double x1 = getWidth() / 2.0;
+		double y1 = getHeight() / 2.0 + RECT_HEIGHT;
+		GLine center = new GLine(x0, y0, x1, y1);
+		add(center);
+	}
+	private void drawRight() {
+		double x0 = getWidth() / 2.0;
+		double y0 = getHeight() / 2.0;
+		double x1 = getWidth() / 2.0 + (RECT_WIDTH + BOX_BUFFER);
+		double y1 = getHeight() / 2.0 + RECT_HEIGHT;
+		GLine right = new GLine(x0, y0, x1, y1);
+		add(right);
+	}
+	private void graphicsProgram() {
+		double x = leftOfCenter() - RECT_WIDTH - BOX_BUFFER;
+		double y = getHeight() / 2.0 + RECT_HEIGHT;
 		GLabel label = new GLabel("GraphicsProgram", x, y);
 		add(label);
 		double x1 = label.getWidth() / 2.0;
 		double y1 = label.getAscent() / 2.0;
-		label.move(-x1, -y1);
+		label.move(RECT_WIDTH / 2.0 - x1, RECT_HEIGHT / 2.0 + y1);
 	}
-	
-	public void consoleProgram() {
-		double x = getWidth() / 2.0;
-		double y = getHeight() / 1.725;
+	private void consoleProgram() {
+		double x = leftOfCenter();
+		double y = getHeight() / 2.0 + RECT_HEIGHT;
 		GLabel label = new GLabel("ConsoleProgram", x, y);
 		add(label);
 		double x1 = label.getWidth() / 2.0;
 		double y1 = label.getAscent() / 2.0;
-		label.move(-x1, -y1);
+		label.move(RECT_WIDTH / 2.0 - x1, RECT_HEIGHT / 2.0 + y1);
 	}
-	
-	public void dialogProgram() {
-		double x = getWidth() / 2.0 + RECT_WIDTH + 20;
-		double y = getHeight() / 1.725;
+	private void dialogProgram() {
+		double x = leftOfCenter() + RECT_WIDTH + BOX_BUFFER;
+		double y = getHeight() / 2.0 + RECT_HEIGHT;
 		GLabel label = new GLabel("DialogProgram", x, y);
 		add(label);
 		double x1 = label.getWidth() / 2.0;
 		double y1 = label.getAscent() / 2.0;
-		label.move(-x1, -y1);
+		label.move(RECT_WIDTH / 2.0 - x1, RECT_HEIGHT / 2.0 + y1);
 	}
-	
-	public void bottomLabels() {
+	private void bottomLabels() {
 		graphicsProgram();
 		consoleProgram();
 		dialogProgram();
 	}
-
-	public void drawProgramHierarchy() {
-		drawTopRow();
-		drawBottomRow();
-		drawLines();
-		topLabel();
-		bottomLabels();
-	}
 }
-
