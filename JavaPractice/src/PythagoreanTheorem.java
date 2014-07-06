257c4b6d9c0a282ff95c43768ae2a75167338b6c
AmishLou <rybread354@aol.com>
2014-07-06 08:05:10 -0500

    New stuff

---
+++ JavaPractice/src/PythagoreanTheorem.java
@@ -0,0 +1,23 @@
+/*
+ * File: PythagoreanTheorem.java
+ * Name: 
+ * Section Leader: 
+ * -----------------------------
+ * This file is the starter file for the PythagoreanTheorem problem.
+ */
+
+import acm.program.*;
+
+public class PythagoreanTheorem extends ConsoleProgram {
+	public void init() {
+		setSize(400, 200);
+	}
+	
+	public void run() {
+		println("Enter values to compute Pythagorean theorem.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = (Math.sqrt((a * a) + (b * b)));
+		println("c = " + c);
+	}
+}
