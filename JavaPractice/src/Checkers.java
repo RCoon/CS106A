e062d1af57df7eb94c7f98c21fb504a24a444e4d
AmishLou <rybread354@aol.com>
2014-10-23 22:37:15 -0700

    Commit

--- JavaPractice/src/Checkers.java
+++ JavaPractice/src/Checkers.java
@@ -44,7 +44,7 @@ public class Checkers extends ConsoleProgram {
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
 				print(board[row][col]);
-				print("  ");
+				print(" ");
 				
 			}
 			println("");
